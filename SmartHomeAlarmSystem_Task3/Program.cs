57df13cba1da5c73e5a131170e2cd7100c0e249c
XetayiMemmedov <khatayirm@code.edu.az>
2025-04-29 13:41:25 +0400

    Added Email Notification

--- SmartHomeAlarmSystem_Task3/Program.cs
+++ SmartHomeAlarmSystem_Task3/Program.cs
@@ -1,4 +1,4 @@
-﻿using static SmartHomeAlarmSystem_Task3.Sensor;
+﻿using System.Net.Mail;
 
 namespace SmartHomeAlarmSystem_Task3
 {
@@ -8,90 +8,167 @@ namespace SmartHomeAlarmSystem_Task3
 
         static void Main(string[] args)
         {
+            var eventAggregator = new EventAggregator();
+
             DoorSensor doorSensor = new DoorSensor("Door");
             WindowSensor windowSensor = new WindowSensor("Window");
             MotionSensor motionSensor = new MotionSensor("Motion");
-            doorSensor.AlarmTriggered += AlarmTriggeredMethod;
-            windowSensor.AlarmTriggered += AlarmTriggeredMethod;
-            motionSensor.AlarmTriggered += AlarmTriggeredMethod;
-            doorSensor.TriggerAlarm();
-            windowSensor.TriggerAlarm();
-            motionSensor.TriggerAlarm();
-            Console.WriteLine("----Alarm Records----");
+
+            eventAggregator.Subscribe<AlarmTriggeredEventArgs>(AlarmTriggeredMethod);
+
+            doorSensor.TriggerAlarm(eventAggregator);
+            windowSensor.TriggerAlarm(eventAggregator);
+            motionSensor.TriggerAlarm(eventAggregator);
+
+            Console.WriteLine("---- Alarm Records ----");
             foreach (var alarm in alarms)
             {
                 Console.WriteLine(alarm.ToString());
             }
-        }
-        public static void AlarmTriggeredMethod(string sensorType, DateTime triggeredAt)
-        {
-            if(sensorType== "Door"|| sensorType == "Window")
-            Console.WriteLine($"{sensorType} is opened at {triggeredAt}");
-            if (sensorType == "Motion")
-            Console.WriteLine($"{sensorType} is detected at {triggeredAt}");
-            alarms.Add(new AlarmRecord(sensorType, triggeredAt));
-            
 
+            eventAggregator.UnSubscribe<AlarmTriggeredEventArgs>(AlarmTriggeredMethod);
+            Console.WriteLine("\nUnsubscribed from event.\n");
+
+            doorSensor.TriggerAlarm(eventAggregator);
+
+            SendEmailNotification("Alarm triggered!", "An alarm was triggered on your smart home system.");
+        }
+
+        public static void AlarmTriggeredMethod(AlarmTriggeredEventArgs args)
+        {
+            alarms.Add(new AlarmRecord(args.SensorType, args.TriggeredAt));
+
+            Console.WriteLine($"{args.SensorType} sensor alarmed at {args.TriggeredAt}");
+
+            SendEmailNotification($"{args.SensorType} Alarm", $"{args.SensorType} sensor triggered at {args.TriggeredAt}");
+        }
+
+        public static void SendEmailNotification(string subject, string body)
+        {
+            try
+            {
+                var smtpClient = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new System.Net.NetworkCredential("xetayi1988@gmail.com", "ypny pxqi jler uqnd"),
+                    EnableSsl = true,
+                };
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress("xetayi1988@gmail.com"),
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = false,
+                };
+                mailMessage.To.Add("khatayirm@code.edu.az");
+
+                smtpClient.Send(mailMessage);
+
+                Console.WriteLine($"Email sent successfully: {subject}");
+            }
+            catch (SmtpException smtpEx)
+            {
+                Console.WriteLine($"SMTP Error: {smtpEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending email: {ex.Message}");
+            }
+        }
+
+        public class AlarmTriggeredEventArgs : EventArgs
+        {
+            public string SensorType { get; }
+            public DateTime TriggeredAt { get; }
+
+            public AlarmTriggeredEventArgs(string sensorType, DateTime triggeredAt)
+            {
+                SensorType = sensorType;
+                TriggeredAt = triggeredAt;
+            }
+        }
+
+        public class Sensor
+        {
+            public string SensorType { get; set; }
+
+            public Sensor(string sensorType)
+            {
+                SensorType = sensorType;
+            }
+
+            public virtual void TriggerAlarm(EventAggregator eventAggregator)
+            {
+                eventAggregator.Publish(new AlarmTriggeredEventArgs(SensorType, DateTime.Now));
+            }
+        }
+
+        public class DoorSensor : Sensor
+        {
+            public DoorSensor(string sensorType) : base(sensorType) { }
+        }
+
+        public class WindowSensor : Sensor
+        {
+            public WindowSensor(string sensorType) : base(sensorType) { }
+        }
+
+        public class MotionSensor : Sensor
+        {
+            public MotionSensor(string sensorType) : base(sensorType) { }
+        }
+        public class AlarmRecord
+        {
+            public string SensorType { get; set; }
+            public DateTime DateTime { get; set; }
+
+            public AlarmRecord(string sensorType, DateTime triggeredAt)
+            {
+                SensorType = sensorType;
+                DateTime = triggeredAt;
+            }
+
+            public override string ToString()
+            {
+                return $"{SensorType} sensor alarmed at {DateTime}";
+            }
+        }
+
+        public class EventAggregator
+        {
+            private readonly Dictionary<Type, List<Delegate>> _eventHandlers = new();
+
+            public void Subscribe<TEvent>(Action<TEvent> eventHandler)
+            {
+                var eventType = typeof(TEvent);
+                if (!_eventHandlers.ContainsKey(eventType))
+                {
+                    _eventHandlers[eventType] = new List<Delegate>();
+                }
+                _eventHandlers[eventType].Add(eventHandler);
+            }
+
+            public void UnSubscribe<TEvent>(Action<TEvent> eventHandler)
+            {
+                var eventType = typeof(TEvent);
+                if (_eventHandlers.ContainsKey(eventType))
+                {
+                    _eventHandlers[eventType].Remove(eventHandler);
+                }
+            }
+
+            public void Publish<TEvent>(TEvent eventData)
+            {
+                var eventType = typeof(TEvent);
+                if (_eventHandlers.ContainsKey(eventType))
+                {
+                    foreach (var handler in _eventHandlers[eventType])
+                    {
+                        ((Action<TEvent>)handler)(eventData);
+                    }
+                }
+            }
         }
     }
-
-    public class Sensor
-    {
-        public string SensorType { get; set; }
-
-        public Sensor(string sensorType)
-        {
-            SensorType = sensorType;
-        }
-
-        public delegate void AlarmTriggeredHandler(string sensorType, DateTime triggeredAt);
-
-        public event AlarmTriggeredHandler? AlarmTriggered;
-
-        public virtual void TriggerAlarm()
-        {
-            AlarmTriggered?.Invoke(SensorType, DateTime.Now);
-        }
-
-    }
-
-    public class DoorSensor : Sensor
-    {
-        public DoorSensor(string sensorType) : base(sensorType)
-        {
-        }
-       
-    }
-    public class WindowSensor : Sensor
-    {
-        public WindowSensor(string sensorType) : base(sensorType)
-        {
-
-        }
-        
-    }
-    public class MotionSensor : Sensor
-    {
-        public MotionSensor(string sensorType) : base(sensorType)
-        {
-        }
-        
-    }
-
-    public class AlarmRecord
-    {
-        public AlarmRecord(string sensorType, DateTime triggeredAt)
-        {
-            SensorType = sensorType;
-            DateTime = triggeredAt;
-        }
-
-        public string SensorType { get; set; }
-        public DateTime DateTime { get; set; }
-        public override string ToString()
-        {
-            return $"{SensorType} sensor alarmed at {DateTime}";
-        }
-    }
-
 }
